1e6c666a3dd18d509f745f4c7bf82cb8a35d85ed
AndrewClev <56375097+AndrewClev@users.noreply.github.com>
2019-10-11 20:13:11 -0500

    Add files via upload

--- codeRoomApp/src/app/app.module.ts
+++ codeRoomApp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GroupComponent } from './group/group.component';
+import { FormatterComponent } from './formatter/formatter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroupComponent,
+    FormatterComponent
   ],
   imports: [
     BrowserModule,
