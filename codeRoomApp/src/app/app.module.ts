c9f4b4ab6d0984332b89754de0432d368ee57a72
AndrewClev <56375097+AndrewClev@users.noreply.github.com>
2019-11-04 03:37:30 -0600

    Add files via upload

--- codeRoomApp/src/app/app.module.ts
+++ codeRoomApp/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { GroupComponent } from './group/group.component';
 import { FormatterComponent } from './formatter/formatter.component';
 import { HomeComponent } from './home/home.component';
+import { CreateGroupComponent } from './create-group/create-group.component';
+import { HelpComponent } from './help/help.component';
+import { MyGroupsComponent } from './my-groups/my-groups.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GroupComponent,
     FormatterComponent,
-    HomeComponent
+    HomeComponent,
+    CreateGroupComponent,
+    HelpComponent,
+    MyGroupsComponent
   ],
   imports: [
     BrowserModule,
