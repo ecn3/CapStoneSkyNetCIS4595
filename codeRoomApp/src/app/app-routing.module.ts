99918090f7f60caf539a256af4c7dc4de67b7679
ecn3 <43419606+ecn3@users.noreply.github.com>
2019-11-04 15:02:06 -0600

    formmatter added

--- codeRoomApp/src/app/app-routing.module.ts
+++ codeRoomApp/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { FormatterComponent } from './formatter/formatter.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent }, 
+  { path: 'formatter', component: FormatterComponent }, 
   { path: 'login', component: LoginComponent }, 
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login'}
