098918e7cfebe980074ae205cb192db0867630f2
ecn3 <43419606+ecn3@users.noreply.github.com>
2019-11-30 17:11:19 -0600

    new methods properly spacd

--- codeRoomApp/src/app/formatter/formatter.component.ts
+++ codeRoomApp/src/app/formatter/formatter.component.ts
@@ -11,14 +11,12 @@ export class FormatterComponent implements OnInit {
   line;
   code = "";
   spaceTriggred = 0;
-  textValue = 'public class test{public static void main( String args[] ){System.out.println("Hello world");}}';
-  formatted = 'public class test {'+ '\n'+
-  '    public static void main(String args[]) {'+'\n'+
-  '        System.out.println("Hello world");'+'\n'+
-  '    }'+'\n'+'}';
+  textValue = 'public class test {public static void main( String args[] ){System.out.println("Hello world");System.out.println("Hello world");}}';
 
-  formatText(): void {
-  this.line = this.textValue.split("{");
+  formatText(textValue): void {
+  this.code = '';
+  this.spaceTriggred = 0;
+  this.line = textValue.split("{");
 
   var lengthOfLine = this.line.length;
 
@@ -37,10 +35,15 @@ export class FormatterComponent implements OnInit {
   }
 
   this.line = this.code.split("}");
-
+  this.spaceTriggred = 0;
+  lengthOfLine = this.line.length;
   this.code = '';
 
   for(let i = 0; i < lengthOfLine; i ++){
+    for(let j = 0; j < this.spaceTriggred; j ++){
+      this.code += '     ';
+      }
+      this.spaceTriggred++;
     if(i < lengthOfLine-1){
       this.code += this.line[i] + '\n'+ '}';
       }else{
@@ -48,6 +51,19 @@ export class FormatterComponent implements OnInit {
   }
   }
 
+  
+  this.line = this.code.split(";");
+  lengthOfLine = this.line.length;
+  if(lengthOfLine > 0){
+  this.code = '';
+  for(let i = 0; i < lengthOfLine; i ++){
+    if(i < lengthOfLine-1){
+    this.code += this.line[i]+ ';' +'\n'; }else{
+    this.code += this.line[i] + '\n';
+    }
+  }
+}
+  
 
 
 }
