7f06dbf297affd712ed0e6bfa8e77d65d7770ac8
ecn3 <43419606+ecn3@users.noreply.github.com>
2019-11-30 18:37:43 -0600

    basics of button

--- codeRoomApp/src/app/home/home.component.ts
+++ codeRoomApp/src/app/home/home.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  groupOwnerEmail='';
+
+  joinGroup(groupOwnerEmail): void {
+    this.groupOwnerEmail = groupOwnerEmail;
+    location.href = "/post_view";
+  }
 
   constructor() { }
 
